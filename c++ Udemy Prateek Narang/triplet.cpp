1f4525f352bc2d8dc52f20db7413af2fb13c8e6e
Aditya <adityakumar110799@gmail.com>
2022-10-15 01:22:20 +0530

    triplet

---
+++ c++ Udemy Prateek Narang/triplet.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(),nums.end());
+        for(int i=0; i< nums.size()-3; i++){
+            int j = i+1;
+            int k = nums.size()-1;
+            while(j<k){
+                if(nums[i]+nums[j]+nums[k]==0){
+                    result.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                    
+                }
+                else if(nums[i]+nums[j]+nums[k]>0){
+                    k--;
+                }
+                else{
+                    j++;
+                }
+            }
+        }
+        return result;
+            
+    }
+};
